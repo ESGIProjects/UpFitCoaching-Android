05316019a05c3039e483b7932a064c1a88433e6a
Kévin Le <kevints.le@gmail.com>
2018-04-16 10:20:22 +0200

    Add UserRealm Object

---
+++ app/src/main/java/com/mycoaching/mycoaching/Util/Model/UserRealm.java
@@ -0,0 +1,77 @@
+package com.mycoaching.mycoaching.Util.Model;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class UserRealm extends RealmObject {
+
+    @PrimaryKey
+    int id;
+
+    private String type, mail, firstName, lastName, birthDate, city, phoneNumber;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
