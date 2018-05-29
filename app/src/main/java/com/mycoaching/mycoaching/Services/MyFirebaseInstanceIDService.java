2df6270e35c9cfabf5073c254cace373e6deaad7
Kévin Le <kevints.le@gmail.com>
2018-05-29 21:00:41 +0200

    Add first implementation of Firebase Messaging

---
+++ app/src/main/java/com/mycoaching/mycoaching/Services/MyFirebaseInstanceIDService.java
@@ -0,0 +1,26 @@
+package com.mycoaching.mycoaching.Services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by kevin on 28/05/2018.
+ */
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService{
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+        sendRegistrationToServer(refreshedToken);
+    }
+
+        private void sendRegistrationToServer(String token) {
+            // TODO
+        }
+
+}
