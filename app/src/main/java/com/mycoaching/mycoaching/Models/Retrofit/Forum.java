7407d7a4f6d2d3d7ba7f839cc7ad3aa3ad842a8f
Kévin Le <kevints.le@gmail.com>
2018-07-01 13:10:38 +0200

    Add Appraisal feature + update UI + minor fixes

---
+++ app/src/main/java/com/mycoaching/mycoaching/Models/Retrofit/Forum.java
@@ -0,0 +1,32 @@
+package com.mycoaching.mycoaching.Models;
+
+/**
+ * Created by kevin on 16/05/2018.
+ */
+public class Forum {
+
+    private int id;
+    private String title;
+
+    public Forum(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
---
+++ app/src/main/java/com/mycoaching/mycoaching/Models/Retrofit/Thread.java
@@ -0,0 +1,78 @@
+package com.mycoaching.mycoaching.Models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.mycoaching.mycoaching.Models.Retrofit.UserRetrofit;
+
+/**
+ * Created by kevin on 16/05/2018.
+ */
+public class Thread {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+
+    @SerializedName("forum")
+    @Expose
+    private Forum forum;
+
+    @SerializedName("lastUpdated")
+    @Expose
+    private String lastUpdated;
+
+    @SerializedName("lastUser")
+    @Expose
+    private UserRetrofit lastUser;
+
+
+    public Thread(int id, String title, Forum forum) {
+        this.id = id;
+        this.title = title;
+        this.forum = forum;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Forum getForum() {
+        return forum;
+    }
+
+    public void setForum(Forum forum) {
+        this.forum = forum;
+    }
+
+    public String getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(String lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public UserRetrofit getLastUser() {
+        return lastUser;
+    }
+
+    public void setLastUser(UserRetrofit lastUser) {
+        this.lastUser = lastUser;
+    }
+}
