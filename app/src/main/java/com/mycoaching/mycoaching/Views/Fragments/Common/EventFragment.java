11926550272e18967ae3d0a1fd898b4f27de3c40
Kévin Le <kevints.le@gmail.com>
2018-07-12 00:28:23 +0200

    Add JWT library

--- app/src/main/java/com/mycoaching/mycoaching/Views/Fragments/Common/EventFragment.java
+++ app/src/main/java/com/mycoaching/mycoaching/Views/Fragments/Common/EventFragment.java
@@ -10,11 +10,13 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.auth0.android.jwt.JWT;
 import com.mycoaching.mycoaching.Api.ApiCall;
 import com.mycoaching.mycoaching.Api.ApiResults;
 import com.mycoaching.mycoaching.Api.ServiceResultListener;
